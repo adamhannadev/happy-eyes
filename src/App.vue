66a5cf79879a9e8388389e7670edd1a3185d5bac
Adam Hanna <adamhannadev@gmail.com>
2024-02-14 13:04:03 -0800

    Use Local Storage to save values

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-import { RouterLink, RouterView } from 'vue-router';
-const today = new Date();
+const today = new Date().toDateString();
 const treatments = ref([
   {name: "Preservative Free Artificial Tears", key: "1", colour: "green"},
   {name: "AM Warm Compress/Mask", key: "2", colour: "pink"},
@@ -10,17 +9,26 @@ const treatments = ref([
   {name: "Omega Supplements", key: "5", colour: "gold"},
   {name: "PM Warm Compress/Mask", key: "6", colour: "purple"},
 ]);
-const selectedTreatments = ref(['']);
+const selectedTreatments = ref([]);
 const eyeStates = ref([
   {name: "Gritty", key: "1", colour: "grey"},
   {name: "Irritated", key: "2", colour: "red"},
   {name: "Comfortable", key: "2", colour: "blue"},
 ]);
-const selectedEyeStates = ref('[]')
+const selectedEyeStates = ref([])
 
+function save(e) {
+  const status = {"treatments": selectedTreatments.value, "eyeStates": "bon"}
+  // const status = {bill: "named Jeremy", arron: "named william"}
+ localStorage.setItem(today, JSON.stringify(status));
+};
+
+const storedData = localStorage.getItem(today);
 </script>
 
 <template>
+  {{ JSON.stringify(selectedTreatments) }}
+  {{ JSON.parse(storedData).treatments }}
   <div class="flex mt-5">
     <div class="flex flex-1"></div>
     <Panel class="flex-grow-1 justify-content-center shadow-1">
@@ -36,7 +44,7 @@ const selectedEyeStates = ref('[]')
     </template>
     <template #icons style="display:none;"></template> 
     <div v-for="treatment of treatments" :key="treatment.key" class="flex flex-column gap-4">
-      <div class="flex align-items-left mb-2">
+      <div class="flex align-items-left mb-4">
         <Checkbox v-model="selectedTreatments" :inputId="treatment.key" name="treatment" :value="treatment.name" />
         <label :style="{color: treatment.colour}" class="ml-2 text-xl" :for="treatment.key">{{ treatment.name }}</label>
       </div>
@@ -45,15 +53,15 @@ const selectedEyeStates = ref('[]')
       <span class="font-bold border-primary border-3 border-round p-1 text-2xl text-primary-800">Today My Eyes Felt Mostly</span>
     </div>
     <div v-for="eyeState of eyeStates" :key="eyeState.key" class="flex flex-column gap-4">
-      <div class="flex align-items-left mb-2">
+      <div class="flex align-items-left mb-4">
         <Checkbox v-model="selectedEyeStates" :inputId="eyeState.key" name="treatment" :value="eyeState.name" />
         <label :style="{color: eyeState.colour}" class="ml-2 text-xl" :for="eyeState.key">{{ eyeState.name }}</label>
       </div>
     </div>
     <div class="flex justify-content-center">
-      <Button label="Submit" class="shadow-1 text-xl" />
+      <Button label="Submit" @click="save" class="shadow-1 text-xl" />
     </div>
-    <Image src="/logo.jpg" alt="Image" width="250" />
+    <!---<Image src="/logo.jpg" alt="Image" width="250" /> -->
     </Panel>
     <div class="flex flex-1"></div>
   </div>
